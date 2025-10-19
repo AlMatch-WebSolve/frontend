3f49d157f84e91c8bc107c76bc3c53bcb2f1ec1e
Hyunjin Yoo <hjin1481@naver.com>
2025-10-19 15:04:30 +0900

    feat: 문제 상세 조회

--- src/pages/SolvePage/SolvePage.jsx
+++ src/pages/SolvePage/SolvePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import SolveSidebar from '../../components/solve/SolveSidebar';
 import CodingPanel from '../../components/solve/CodingPanel/CodingPanel';
 import ResultPanel from '../../components/solve/ResultPanel/ResultPanel';
@@ -6,17 +7,22 @@ import ProblemPanel from '../../components/problems/ProblemPanel/ProblemPanel';
 import styles from './SolvePage.module.css';
 
 function SolvePage() {
+  const { problemId } = useParams();
   const [codingTab, setCodingTab] = useState('code');
+
+  const [resultActiveTab, setResultActiveTab] = useState('test');
 
   return (
     <div className={styles.ideLayout}>
       <SolveSidebar />
       <div className={styles.problemContainer}>
-        <ProblemPanel />
+        <ProblemPanel problemId={Number(problemId)} />
       </div>
       <div className={styles.solveContainer}>
         <CodingPanel onTabChange={setCodingTab} />
-        {codingTab === 'code' ? <ResultPanel /> : null}
+        {codingTab === 'code' ? (
+          <ResultPanel activeTab={resultActiveTab} />
+        ) : null}
       </div>
     </div>
   );
--- src/pages/WorkspacePage/WorkspacePage.jsx
+++ src/pages/WorkspacePage/WorkspacePage.jsx
@@ -5,9 +5,9 @@ function WorkspacePage() {
     <div>
       <h1>워크스페이스 페이지</h1>
       <p>내 문제/폴더 목록이 보이는 곳입니다.</p>
-      <Link to='/solve/123'>
+      <Link to='/solve/2741'>
         {' '}
-        {/* 123은 임시 문제 ID */}
+        {/* 2741은 임시 문제 ID */}
         <button>A+B 문제 풀러가기</button>
       </Link>
     </div>
