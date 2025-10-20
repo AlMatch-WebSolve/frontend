649441b382b38cbfef21d28aafb980a5e780d7c3
Hyunjin Yoo <hjin1481@naver.com>
2025-10-21 00:04:24 +0900

    fix: 프록시 제거

--- vite.config.js
+++ vite.config.js
@@ -10,23 +10,6 @@ export default ({ mode }) => {
     define: {
       global: 'window',
     },
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          secure: false, // HTTPS 인증서가 self-signed일 경우 필요
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-
-        '/ws': {
-          target: env.VITE_WS_URL,
-          ws: true,
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
     css: {
       modules: {
         localsConvention: 'camelCase',
